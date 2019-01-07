f8180d1163ac5a00378ab991b51f14dca002dbed
adolfo <agonzalez15@mail.greenriver.edu>
2019-01-07 14:32:34 -0500

    created page

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require autoload
+require_once('vendor/autoload.php');
+
+//create an instance of the BASE CLASS
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function(){
+    echo "<h1>Hello, World!</h1>";
+});
+
+//run fat free framework
+$f3->run();
